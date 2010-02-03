14a2067bb2e258b2f5b5c9a3c958ac26d4bb6908
AlexanderPico <apico@gladstone.ucsf.edu>
2010-02-03 09:30:04 +0000

    works with CyCommands from  Workflows!

--- src/org/genmapp/criteriamapper/CriteriaMapper.java
+++ src/org/genmapp/criteriamapper/CriteriaMapper.java
@@ -8,50 +8,73 @@ package org.genmapp.criteriamapper;
  * to the plugins menu and sets up an action command to fire the dialog window in BooleanSettingsDialog.
  * 
  */
-import java.util.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
+import java.util.Map;
 
-import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
-
-
-
-
-import giny.model.Node;
-import giny.view.NodeView;
-
-
-import cytoscape.plugin.CytoscapePlugin;
-import cytoscape.util.CytoscapeAction;
 import cytoscape.Cytoscape;
-import cytoscape.CyNetwork;
-import cytoscape.CyNode;
-import cytoscape.CyEdge;
-import cytoscape.view.CyNetworkView;
-import cytoscape.data.Semantics;
-
-import cytoscape.layout.*;
+import cytoscape.command.AbstractCommandHandler;
+import cytoscape.command.CyCommandException;
+import cytoscape.command.CyCommandHandler;
+import cytoscape.command.CyCommandManager;
+import cytoscape.command.CyCommandNamespace;
+import cytoscape.command.CyCommandResult;
+import cytoscape.layout.Tunable;
+import cytoscape.plugin.CytoscapePlugin;
 
 
 
 public class CriteriaMapper extends CytoscapePlugin {
 	
+	private JMenuItem item = new JMenuItem("Criteria Mapper");
 	
 	public CriteriaMapper() {
 		
-		JMenuItem item = new JMenuItem("Criteria Mapper");
 		
-		item.addActionListener(new CriteriaMapperCommandListener());
-		
-		JMenu pluginMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar().getMenu("Plugins");
-		
+		item.addActionListener(new CriteriaMapperCommandListener());		
+		JMenu pluginMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar().getMenu("Plugins");	
 		pluginMenu.add(item);
 		
+		// register cycommands
+		try {
+			// You must reserve your namespace first
+			CyCommandNamespace ns = CyCommandManager
+					.reserveNamespace("criteria mapper");
+			// Now register this handler as handling "open"
+			CyCommandHandler oh = new OpenHandler(ns);
+		} catch (RuntimeException e) {
+			// Handle already registered exceptions
+			System.out.println(e);
+		}
 		
+	}
+
+	class OpenHandler extends AbstractCommandHandler {
+		protected OpenHandler(CyCommandNamespace ns) {
+			super(ns);
+			addArgument("open");
+		}
+
+		public String getHandlerName() {
+			return "open";
+		}
+
+		public CyCommandResult execute(String command, Map<String, Object> args)
+				throws CyCommandException {
+			item.getActionListeners()[0].actionPerformed(null);
+			return new CyCommandResult();
+		}
+
+		public CyCommandResult execute(String arg0, Collection<Tunable> arg1)
+				throws CyCommandException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
 	}
 	
 	
